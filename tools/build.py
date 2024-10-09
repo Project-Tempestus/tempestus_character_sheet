e107e39e039759320f43103712db7333147bdbd9
Michal Czyz <michal.kacper.czyz@gmail.com>
2024-10-09 21:56:36 +0200

    Cleanup build
    Signed-off-by: Michal Czyz <michal.kacper.czyz@gmail.com>

--- tools/build.py
+++ tools/build.py
@@ -1,10 +1,17 @@
 import yaml
 import os
 import argparse
-from jinja2 import Template, Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 import common
+from pathlib import Path
+
 
 def load_yml(path):
+    """
+    Every YAML file used should have an array ITEM_OPTIONS,
+    the length of this array will be added to the yaml_text,
+    which is used by the jinja template.
+    """
     with open(path, "r", encoding="utf-8") as file:
         yaml_text = yaml.safe_load(file)
     ITEM_OPTIONS_LEN = len(yaml_text["ITEM_OPTIONS"])
@@ -13,36 +20,33 @@ def load_yml(path):
 
 
 def load_template(path):
+    """
+    Loads jinja template from path
+    """
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template(path)
     return template
-
-
-def render_template(template, data):
-    content = template.render(data)
-    return content
 
 
 def build_elements(filesDict, templatePath):
     elements_dict = filesDict.copy()
     template = load_template(path=templatePath)
     for key, value in filesDict.items():
-        yaml_text = load_yml(path="data/"+value)
-        # TODO: Dirty hack for special skill case
+        yaml_text = load_yml(path="data/" + value)
+        # FIXME: Dirty hack for special skill case
         if templatePath != "translation_row.j2":
             if key == "SPECIAL_SKILL":
                 template = load_template(path="skill_input.j2")
 
-        elements_dict[key] = render_template(template=template,data=yaml_text)
-
+        elements_dict[key] = template.render(yaml_text)
 
     return elements_dict
 
 
-def build_file(outputFile, filesDict, entryTemplate, fullTemplate ):
+def build_file(outputFile, filesDict, entryTemplate, fullTemplate):
     elements_dict = build_elements(filesDict, templatePath=entryTemplate)
-    template = load_template(path = fullTemplate)
-    content = render_template(template=template, data=elements_dict)
+    template = load_template(path=fullTemplate)
+    content = template.render(elements_dict)
     with open(outputFile, "w", encoding="utf-8") as file:
         file.write(content)
     file.close()
@@ -50,21 +54,24 @@ def build_file(outputFile, filesDict, entryTemplate, fullTemplate ):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Optional app description')
-    parser.add_argument('--output_dir', required=False, type=str, help='Output directory', default="../release/")
+    parser = argparse.ArgumentParser(description="Optional app description")
+    parser.add_argument(
+        "--output_dir",
+        required=False,
+        type=str,
+        help="Output directory",
+        default="../release/",
+    )
     args = parser.parse_args()
 
-    outputDir=args.output_dir
-    if not os.path.exists(outputDir):
-        os.makedirs(outputDir)
+    outputDir = Path(args.output_dir)
+    translations_dir = outputDir / "translations"
 
-    outputHTML = os.path.join(outputDir,"project_tempestus.html")
-
-    translations_dir = os.path.join(outputDir,"translations")
     if not os.path.exists(translations_dir):
         os.makedirs(translations_dir)
-    outputJSON = os.path.join(translations_dir,"pl.json")
+
+    outputHTML = outputDir / "project_tempestus.html"
+    outputJSON = translations_dir / "pl.json"
 
     build_file(outputHTML, common.FILES_DICT, "sheet_row.j2", "character_sheet.j2")
     build_file(outputJSON, common.FILES_DICT, "translation_row.j2", "pl.j2")
-
